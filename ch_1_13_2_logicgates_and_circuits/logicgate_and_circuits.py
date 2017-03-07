fab9cdb094db6c245f6fa114e0aea643e062ad17
carlb15 <carlb15@carlb15s-MacBook-Air.local>
2017-03-06 20:07:47 -0800

    Nand and Nor Gates

--- ch_1_13_2_logicgates_and_circuits/logicgate_and_circuits.py
+++ ch_1_13_2_logicgates_and_circuits/logicgate_and_circuits.py
@@ -1,38 +1,38 @@
 class LogicGate:
-  
+
   def __init__(self, n):
     self.label  = n
     self.output = None
-    
+
   def getLabel(self):
     return self.label
-    
+
   def getOutput(self):
     self.output = self.performGateLogic()
     return self.output
-        
+
 class BinaryGate(LogicGate):
-  
+
   def __init__(self, n):
     # initializing the inherited classes data items
-    LogicGate.__init__(self, n) 
+    LogicGate.__init__(self, n)
     # Above can rewritten as super(BinaryGate, self).__init__(n)
-    
+
     self.pinA = None
     self.pinB = None
-    
+
   def getPinA(self):
-    if self.pinA == None:    
+    if self.pinA == None:
       return int(input("Enter Pin A input for gate " + self.getLabel() + "-->"))
-    
+
     return self.pinA.getFrom().getOutput()
-    
+
   def getPinB(self):
     if self.pinB == None:
       return int(input("Enter Pin B input for gate " + self.getLabel() + "-->"))
-    
+
     return self.pinB.getFrom().getOutput()
-  
+
   def setNextPin(self, source):
     if self.pinA == None:
       self.pinA = source
@@ -40,77 +40,86 @@ class BinaryGate(LogicGate):
       self.pinB = source
     else:
       raise RuntimeError("Error: NO EMPTY PINS")
-        
+
 class UnaryGate(LogicGate):
-  
+
   def __init__(self, n):
     # initializing the inherited classes data items
     LogicGate.__init__(self, n)
     # Above can rewritten as super(UnaryGate, self).__init__(n)
-    
+
     self.pin = None
-    
+
   def getPin(self):
     if self.pin == None:
-      return int(input("Enter Pin input for gate " + self.getLabel() + "-->"))    
+      return int(input("Enter Pin input for gate " + self.getLabel() + "-->"))
     else:
       return self.pin.getFrom().getOutput()
-    
+
   def setNextPin(self, source):
     if self.pin == None:
       self.pin = source
     else:
       raise RuntimeError("Cannot Connect: NO EMPTY PINS on this gate.")
-    
-    
+
 class AndGate(BinaryGate):
-  
+
   def __init__(self, n):
     # Initializing the parent classes data items
-    BinaryGate.__init__(self, n)    
+    BinaryGate.__init__(self, n)
 
   def performGateLogic(self):
-    
+
     a = self.getPinA()
     b = self.getPinB()
-    
+
     return ((a == 1) and (b == 1))
-    
+
 class OrGate(BinaryGate):
-   
+
   def __init__(self, n):
     BinaryGate.__init__(self, n)
-    
+
   def performGateLogic(self):
     a = self.getPinA()
     b = self.getPinB()
-    
+
     return ((a == 1) or (b == 1))
-           
+
 class NotGate(UnaryGate):
-  
+
   def __init__(self, n):
     UnaryGate.__init__(self, n)
-    
+
   def performGateLogic(self):
     a = self.getPin()
-    
+
     return (not a)
-   
+
+class NorGate(OrGate):
+
+    def performGateLogic(self):
+        return not (super().performGateLogic())
+
+class NandGate(AndGate):
+
+    def performGateLogic(self):
+        return not (super().performGateLogic())
+
 class Connector:
-  
+
   def __init__(self, fgate, tgate):
     self.fromgate = fgate
     self.togate = tgate
-    
+
     tgate.setNextPin(self)
-    
+
   def getFrom(self):
     return self.fromgate
-    
+
   def getTo(self):
     return self.togate
-        
+
 def main():
   g1 = AndGate("G1")
   g2 = AndGate("G2")
@@ -121,17 +130,11 @@ def main():
   c3 = Connector(g3,g4)
   print(g4.getOutput())
 
+  g5 = NandGate("G5")
+  g6 = NandGate("G6")
+  g7 = AndGate("G7")
+  c4 = Connector(g5, g7)
+  c5 = Connector(g6, g7)
+  print(g7.getOutput())
+
 main()
-
-
-
-
-
-
-
-
-
-
-
-
-
