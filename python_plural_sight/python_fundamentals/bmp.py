0d94fbf581f30e858aec0e30fa0e0ede9893be7e
Carl Barbee <carlb1577@gmail.com>
2017-05-03 16:41:10 -0700

    Compute BMP dimensions

--- python_plural_sight/python_fundamentals/bmp.py
+++ python_plural_sight/python_fundamentals/bmp.py
@@ -93,3 +93,38 @@ def _int32_to_bytes(i):
                   i >> 8 & 0xff,
                   i >> 16 & 0xff,
                   i >> 24 & 0xff))
+
+
+def _bytes_to_int32(b):
+    """Convert a byte object containing four bytes into an integer."""
+    return b[0] | (b[1] << 8) | (b[2] << 16) | (b[3] << 24)
+
+
+def dimensions(filename):
+    """Determine the dimensions in pixels of a BMP image."""
+    """
+    Args:
+        filename: The filename of a BMP file.
+
+    Returns:
+        A tuple containing two integers with the width and height in pixels.
+
+    Raises:
+        ValueError: If the file was nto a BMP file.
+        OSError: If there was a problem reading the file.
+    """
+    with open(filename, 'rb') as f:
+        # First  two magic bytes expected in a BMP file.
+        magic = f.read(2)
+        # Validate first two magic bytes of file are BMP file.
+        if magic != b'BM':
+            raise ValueError("{} is not a BMP file".format(filename))
+
+        # Image dimensions stored 18 bytes in file.
+        f.seek(18)
+        # Width and height bytes of image.
+        width_bytes = f.read(4)
+        height_bytes = f.read(4)
+
+        return (_bytes_to_int32(width_bytes),
+                _bytes_to_int32(height_bytes))
