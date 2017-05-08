7e6e0a6ab85c3096a8103de0fdfdbe4d03932dbe
Carl Barbee <carlb1577@gmail.com>
2017-05-08 16:37:47 -0700

    Binary search on an ordered list.

---
+++ python_school/binary_search.py
@@ -0,0 +1,44 @@
+"""Binary Search in an ordered list."""
+
+
+def binary_search(myList, item):
+    """Binary search through an ordered list."""
+    """
+    Args:
+        myList: ordered list to search
+        item: item to find
+
+    Returns: True if item is found and False if not.
+    Example:
+    """
+    leftIdx = 0
+    rightIdx = len(myList) - 1
+    found = False
+
+    while leftIdx <= rightIdx and not found:
+        middleIdx = (leftIdx + rightIdx) // 2
+        if myList[middleIdx] == item:
+            found = True
+        elif myList[middleIdx] > item:
+            rightIdx = middleIdx - 1
+        else:
+            leftIdx = middleIdx + 1
+    return found
+
+
+if __name__ == "__main__":
+    myList = [2]
+    assert(binary_search(myList, 2))
+    myList = [2, 3]
+    assert(binary_search(myList, 3))
+    assert(not binary_search(myList, 4))
+    assert(not binary_search([], 3))
+    myList = [2, 5, 7, 12, 14, 21, 28, 31, 36]
+    assert(binary_search(myList, 2))
+    assert(binary_search(myList, 7))
+    assert(binary_search(myList, 14))
+    assert(binary_search(myList, 31))
+    assert(binary_search(myList, 36))
+    assert(not binary_search(myList, -2))
+    assert(not binary_search(myList, 16))
+    assert(not binary_search(myList, 100))
