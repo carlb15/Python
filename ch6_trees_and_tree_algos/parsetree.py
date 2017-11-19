3f5e5c06bb61f8060707cb0bf9453b5c4359e7c0
carlb15 <4373509+carlb15@users.noreply.github.com>
2017-11-19 15:53:27 -0500

    Evaluate the parse tree math expression.
    Evaluate function to return the result of the math exp in the parse tree.

--- ch6_trees_and_tree_algos/parsetree.py
+++ ch6_trees_and_tree_algos/parsetree.py
@@ -1,11 +1,14 @@
 from bst import BinaryTree
 from stack import Stack
+import operator as op # handles the evaluating the expression
+
 
 def handleLeftParen(root, nodeStack):
     """Create new left subtree and descend to it."""
     root.insertLeft('')
     nodeStack.push(root)
     return root.getLeftChild()
+
 
 def handleOperator(token, nodeStack, root):
     """Set root value, then create & move to right subtree."""
@@ -14,10 +17,12 @@ def handleOperator(token, nodeStack, root):
     nodeStack.push(root)
     return root.getRightChild()
 
+
 def handleOperand(token, nodeStack, root):
     """Set root value and move to parent."""
     root.setRootVal(int(token))
     return nodeStack.pop()
+
 
 def handleTokens(tokens, nodeStack, root):
     """Process all tokens in mathematical expression."""
@@ -36,6 +41,7 @@ def handleTokens(tokens, nodeStack, root):
 
     return root
 
+
 def buildAParseTree(mathExp):
     """Build Parse tree based on mathematical expression."""
     tokens = mathExp.split()
@@ -44,19 +50,43 @@ def buildAParseTree(mathExp):
     nodeStack.push(root)
 
     try:
-        handleTokens(tokens, nodeStack, root)
+        root = handleTokens(tokens, nodeStack, root)
     except:
         print("Invalid mathematical expression!")
+        root = None
 
     return root
+
+
+def evaluate_expression(parseTree):
+    if parseTree == None:
+        return None
+
+    """Evaluate the mathematical expression and return a result."""
+    opers = {'+':op.add, '-':op.sub, '*':op.mul, '/':op.truediv}
+
+    left = parseTree.getLeftChild()
+    right = parseTree.getRightChild()
+
+    if left and right:
+        fn = opers[parseTree.getRootVal()]
+        return fn(evaluate_expression(left), evaluate_expression(right))
+    else:
+        return parseTree.getRootVal()
+
 
 if __name__=="__main__":
     # Build parse tree with valid mathematical expression.
     mathExp = '( 3 + ( 4 * 5 ) )'
     print(mathExp)
-    buildAParseTree(mathExp)
+    parseTree = buildAParseTree(mathExp)
+    assert(evaluate_expression(parseTree) == 23)
+
+    del parseTree
 
     # Build parse tree with invalid mathematical expression.
     mathExp = '( 3 + ( 4 ] * 5 ) ) }'
     print(mathExp)
-    buildAParseTree(mathExp)
+    parseTree = buildAParseTree(mathExp)
+    assert(parseTree == None)
+    assert(evaluate_expression(parseTree) == None)
